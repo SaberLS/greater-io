3b07f4c51c364af8ff800993488451b8fb011856
SaberLS <patryk.platek19@gmail.com>
2026-01-22 19:33:59 +0100

    feat(server/api): add logout route to auth router

--- server/src/routes/auth/router.ts
+++ server/src/routes/auth/router.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
-import { loginController } from './controllers'
+import { ensureAuthenticated } from '../protected'
+import { loginController, logoutController } from './controllers'
 
 const authRouter = Router()
 
 authRouter.post('/login', loginController)
+authRouter.post('/logout', ensureAuthenticated, logoutController)
 
 export { authRouter }
