7ecc85dd38ef5eaaf8193ec5eb43776f7c7017db
SaberLS <patryk.platek19@gmail.com>
2026-01-23 20:27:57 +0100

    test(server/sockets): add protected sockets integration tests

---
+++ server/__tests__/app/sockets/protected/connect.test.ts
@@ -0,0 +1,40 @@
+import { buildTestServer, TestUser } from '../../../utils'
+
+describe('Protected Socket Namespace', () => {
+  const server = buildTestServer()
+  let user: TestUser
+
+  beforeAll(async () => {
+    await server.init()
+    user = new TestUser(server.url)
+  })
+
+  afterAll(async () => {
+    user.disconnectSocket()
+    await server.close()
+  })
+
+  it('should respond with secure-pong', async () => {
+    await user.login()
+    const socket = await user.connectProtectedSocket()
+
+    await new Promise<void>(resolve => {
+      socket.once('secure-pong', msg => {
+        expect(msg).toBe('secure-pong')
+        resolve()
+      })
+
+      socket.emit('ping')
+    })
+
+    socket.disconnect()
+  })
+
+  it('should reject socket without token', async () => {
+    const unauthUser = new TestUser(server.url)
+
+    await expect(unauthUser.connectProtectedSocket()).rejects.toThrow(
+      'Unauthorized'
+    )
+  })
+})
