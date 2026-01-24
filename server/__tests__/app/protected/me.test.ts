2b9696d7844b8452713f8fecef4bf7ebf0b84b8e
SaberLS <patryk.platek19@gmail.com>
2026-01-24 21:35:47 +0100

    perf(server/test): make only one request before tests

--- server/__tests__/app/protected/me.test.ts
+++ server/__tests__/app/protected/me.test.ts
@@ -1,4 +1,5 @@
 import MESSAGES from '@greater-io/server/src/CONSTS/MESSAGES.json'
+import request from 'supertest'
 import { buildTestServer, createExpectRes, TestUser } from '../../utils'
 
 const expectMe = createExpectRes<[200, 401]>({
@@ -9,10 +10,13 @@ const expectMe = createExpectRes<[200, 401]>({
 describe('GET /protected/me', () => {
   const server = buildTestServer()
   let user: TestUser
+  let res: request.Response
 
   beforeAll(async () => {
     await server.init()
     user = new TestUser(server.url)
+    await user.login()
+    res = await user.me()
   })
 
   afterAll(async () => {
@@ -20,15 +24,10 @@ describe('GET /protected/me', () => {
   })
 
   it('should respond success with valid credentials', async () => {
-    await user.login()
-    const res = await user.me()
-
     expectMe(res).success(200).and.haveMessage()
   })
 
   it('response should have user data', async () => {
-    const res = await user.me()
-
     expect(res.body.user).toHaveProperty('username')
     expect(res.body.user).toHaveProperty('id')
   })
