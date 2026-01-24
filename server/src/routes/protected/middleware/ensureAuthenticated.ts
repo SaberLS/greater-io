f8d0d2a6e24b0bb8990e84841eb12fc9679a68e0
SaberLS <patryk.platek19@gmail.com>
2026-01-24 21:29:44 +0100

    fix(server/auth): add only username and id to req.user instead of full UserDBO

--- server/src/routes/protected/middleware/ensureAuthenticated.ts
+++ server/src/routes/protected/middleware/ensureAuthenticated.ts
@@ -1,22 +1,25 @@
 import type { RequestHandler } from 'express'
 import passport from 'passport'
 import MESSAGES from '../../../CONSTS/MESSAGES.json'
-import type { IUser } from '../../../models/User/User'
+import type { IUserDBO } from '../../../models'
 
 const ensureAuthenticated: RequestHandler = (req, res, next) => {
   passport.authenticate(
     'jwt',
     { session: false },
-    (error_: any, user: IUser) => {
+    (error_: any, user: IUserDBO) => {
       if (error_) return next(error_)
 
       if (!user) {
         return res
           .status(401)
-          .json({ message: MESSAGES.protected.validate[401] })
+          .json({ success: false, message: MESSAGES.protected.validate[401] })
       }
 
-      req.user = user
+      req.user = {
+        username: user.username,
+        id: user.id,
+      }
       next()
     }
   )(req, res, next)
