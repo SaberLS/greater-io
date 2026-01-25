7a9e8b006cb247de41b344b8cf0eae2c7424d2fa
SaberLS <patryk.platek19@gmail.com>
2026-01-25 18:09:49 +0100

    refactor(server/test): simplify createExpectRes by removing e object and moving it's methods to function scope

--- server/__tests__/utils/createExpect.ts
+++ server/__tests__/utils/createExpect.ts
@@ -15,37 +15,43 @@ function createExpectRes<TCodes extends [200 | 201 | 204, ...number[]]>(
   type CodesUnion = TCodes[number]
 
   return (res: request.Response) => {
-    const e = {
-      fail: (status: Exclude<CodesUnion, 200 | 201 | 204>) => {
-        expect(res).not.toBeOk()
-        expect(res).not.toBeSuccessful()
-        expect(res).toHaveStatus(status)
+    const haveMessage = (status: CodesUnion) => {
+      if (!(status in messages)) {
+        throw new Error(`No message defined for status ${status}`)
+      }
 
-        return {
-          and: {
-            haveMessage: () => e.haveMessage(status),
-          },
-        }
-      },
-
-      haveMessage: (status: CodesUnion) => {
-        expect(res).toHaveMessage(messages[status])
-      },
-
-      success: (status: 200 | 201 | 204 = 200) => {
-        expect(res).toBeOk()
-        expect(res).toHaveStatus(status)
-        expect(res).toBeSuccessful()
-
-        return {
-          and: {
-            haveMessage: () => e.haveMessage(status),
-          },
-        }
-      },
+      expect(res).toHaveMessage(messages[status])
     }
 
-    return e
+    const success = (status: 200 | 201 | 204 = 200) => {
+      expect(res).toBeOk()
+      expect(res).toHaveStatus(status)
+      expect(res).toBeSuccessful()
+
+      return {
+        and: {
+          haveMessage: () => haveMessage(status),
+        },
+      }
+    }
+
+    const fail = (status: Exclude<CodesUnion, 200 | 201 | 204>) => {
+      expect(res).not.toBeOk()
+      expect(res).not.toBeSuccessful()
+      expect(res).toHaveStatus(status)
+
+      return {
+        and: {
+          haveMessage: () => haveMessage(status),
+        },
+      }
+    }
+
+    return {
+      haveMessage,
+      fail,
+      success,
+    }
   }
 }
 
