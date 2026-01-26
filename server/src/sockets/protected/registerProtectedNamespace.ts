6a0c48f4223c8d76342d4466efcf7c1314a94167
SaberLS <patryk.platek19@gmail.com>
2026-01-26 15:22:12 +0100

    feat(server/lobby): add socket lobby:create and lobby:join

--- server/src/sockets/protected/registerProtectedNamespace.ts
+++ server/src/sockets/protected/registerProtectedNamespace.ts
@@ -1,9 +1,12 @@
-import { info } from '@greater-io/shared'
 import type { Namespace } from 'socket.io'
+import { Lobby } from '../../services/LobbyManager/Lobby/Lobby'
 import type {
   IoAuthenticatedNamespace,
   IoAuthenticatedSocket,
 } from '../../types/AuthSocket'
+
+import { parseError } from '@greater-io/shared'
+import { LobbyManager } from '../../services/LobbyManager/LobbyManager'
 import { socketJwtAuth } from './socketJWTAuth'
 
 function registerProtectedNamespace(
@@ -11,6 +14,9 @@ function registerProtectedNamespace(
 ): IoAuthenticatedNamespace {
   const protectedNs = namespace.use(socketJwtAuth) as IoAuthenticatedNamespace
 
+  const lobbyManager = new LobbyManager()
+
+  // check if user exists and if user is already connected
   protectedNs.use((socket: IoAuthenticatedSocket, next) => {
     const { user } = socket.data
 
@@ -18,20 +24,69 @@ function registerProtectedNamespace(
 
     if (user.socketId) return next(new Error('User already connected'))
 
+    user.socketId = socket.id
     next()
   })
 
-  protectedNs.on('connection', (socket: IoAuthenticatedSocket) => {
+  protectedNs.on('connection', socket => {
     const user = socket.data.user
-    user.socketId = socket.id
 
     socket.on('ping', () => {
       socket.emit('secure-pong', 'secure-pong')
     })
 
+    socket.on('lobby:create', () => {
+      if (socket.data.lobbyId) {
+        socket.emit('error', 'Already in lobby')
+        return
+      }
+
+      const lobby = new Lobby(user)
+      lobbyManager.add(lobby.id, lobby)
+
+      // associate lobby with socket
+      socket.data.lobbyId = lobby.id
+
+      // join socket.io room for this lobby
+      socket.join(`lobby:${lobby.id}`)
+
+      // emit initial state
+      protectedNs.to(`lobby:${lobby.id}`).emit('lobby:state', lobby.state)
+    })
+
+    socket.on('lobby:join', lobbyId => {
+      if (socket.data.lobbyId) {
+        socket.emit('lobby:error', 'Already in lobby')
+        return
+      }
+
+      const lobby = lobbyManager.get(lobbyId)
+
+      if (!lobby) {
+        socket.emit('lobby:error', `Lobby ${lobbyId} is not available`)
+        return
+      }
+
+      try {
+        lobby.add(user)
+
+        socket.join(`lobby:${lobbyId}`)
+        socket.data.lobbyId = lobbyId
+
+        protectedNs.to(`lobby:${lobbyId}`).emit('lobby:state', lobby.state)
+      } catch (_e: unknown) {
+        const error = parseError(_e)
+
+        socket.emit('lobby:error', error.message)
+      }
+    })
+
+    // socket.on('lobby:leave', () => {})
+    // socket.on('lobby:ready', () => {})
+    // socket.on('lobby:start', () => {})
+
     socket.on('disconnect', () => {
-      info(`Protected Socket disconnected: ${socket.id}, ${user.username}`)
-      user.socketId = undefined
+      // cleanup later
     })
   })
 
