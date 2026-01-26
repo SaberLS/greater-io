08c69147defe6407d35a194b30eef9383cf889c0
SaberLS <patryk.platek19@gmail.com>
2026-01-26 14:51:05 +0100

    fix(server/types): change authenticated namespace types to reflect the real socket state with user and lobbyId

--- server/src/types/AuthSocket.d.ts
+++ server/src/types/AuthSocket.d.ts
@@ -1,29 +1,78 @@
 import type { JwtPayload } from 'jsonwebtoken'
-import type { ExtendedError, Namespace, Socket } from 'socket.io'
+import type {
+  DefaultEventsMap,
+  ExtendedError,
+  Namespace,
+  Socket,
+} from 'socket.io'
 import type { ISocketUser } from '../models/User/ISocketUser'
+import type { LobbyID } from '../services/LobbyManager/Lobby/Lobby'
 
-interface IoSocketData {
-  user?: ISocketUser
+type IoAuthenticatedUser = Omit<ISocketUser, 'socketId'> & { socketId?: string }
+
+interface IoAuthData {
+  user?: IoAuthenticatedUser
 }
 
-interface IoSocket extends Socket {
-  data: IoSocketData
-  auth?: Partial<JwtPayload>
+interface IoSocketBeforeAuth extends Socket<
+  DefaultEventsMap,
+  DefaultEventsMap,
+  DefaultEventsMap,
+  IoAuthData
+> {
+  handshake: Socket['handshake'] & {
+    auth?: Partial<JwtPayload>
+  }
 }
 
-interface IoAuthenticatedSocket extends IoSocket {
+interface IoAuthenticatedSocket extends Omit<IoSocketBeforeAuth, 'data'> {
+  data: {
+    user: IoAuthenticatedUser
+  }
+  handshake: Socket['handshake'] & {
+    auth: JwtPayload
+  }
+}
+
+interface IoAuthenticatedUserSocket extends Socket<
+  AuthenticatedEventsMap,
+  AuthenticatedEventsMap,
+  AuthenticatedEventsMap,
+  {
+    user: ISocketUser
+    lobbyId: LobbyID
+  }
+> {
   data: {
     user: ISocketUser
+    lobbyId: LobbyID
   }
-  auth: JwtPayload
+  handshake: Socket['handshake'] & {
+    auth: JwtPayload
+  }
 }
 
-interface IoAuthenticatedNamespace extends Namespace {
-  sockets: Map<string, IoAuthenticatedSocket>
+interface LobbyUser extends IoAuthenticatedUserSocket {}
+
+interface AuthenticatedEventsMap extends DefaultEventsMap {
+  connection: (socket: IoAuthenticatedUserSocket) => void
+  'lobby:create': () => void
+}
+
+interface IoAuthenticatedNamespace extends Namespace<
+  AuthenticatedEventsMap,
+  AuthenticatedEventsMap,
+  AuthenticatedEventsMap,
+  {
+    user: ISocketUser
+    lobbyId: LobbyID
+  }
+> {
+  sockets: Map<string, IoAuthenticatedUserSocket>
 
   use(
     fn: (
-      socket: IoAuthenticatedSocket,
+      socket: IoAuthenticatedUserSocket,
       next: (err?: ExtendedError) => void
     ) => void
   ): this
@@ -31,8 +80,13 @@ interface IoAuthenticatedNamespace extends Namespace {
   // Overload 1: authenticated socket
   on<T extends string>(
     ev: T,
-    listener: (socket: IoAuthenticatedSocket) => void
+    listener: (socket: IoAuthenticatedUserSocket) => void
   ): this
 }
 
-export type { IoAuthenticatedNamespace, IoAuthenticatedSocket }
+export type {
+  IoAuthenticatedNamespace,
+  IoAuthenticatedSocket,
+  IoAuthenticatedUserSocket,
+  IoSocketBeforeAuth,
+}
