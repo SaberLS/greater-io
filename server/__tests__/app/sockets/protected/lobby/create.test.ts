c8b28232708e3fdefe04d3ad53894b709f6fd01b
SaberLS <patryk.platek19@gmail.com>
2026-01-26 20:27:15 +0100

    refactor(server/test): split lobby tests into create and join test cases

---
+++ server/__tests__/app/sockets/protected/lobby/create.test.ts
@@ -0,0 +1,53 @@
+import type { Socket } from 'socket.io-client'
+import type { IUser } from '../../../../../src/models'
+import { buildTestServer, TestUsers } from '../../../../utils'
+
+describe('Protected Socket Namespace lobby:create', () => {
+  const server = buildTestServer()
+
+  let users: TestUsers
+  let aliceSocket: Socket
+  let aliceData: IUser
+
+  beforeAll(async () => {
+    await server.init()
+    users = new TestUsers(server.url)
+
+    const alice = await users.getLoggedUser('alice')
+    aliceData = (await alice.me()).body.data
+    aliceSocket = await alice.connectProtectedSocket()
+  })
+
+  afterAll(async () => {
+    await server.close()
+    aliceSocket.disconnect()
+  })
+
+  it('should create a lobby successfully', async () => {
+    // Listen for the lobby state after creation
+    const state = await new Promise<any>(resolve => {
+      aliceSocket.once('lobby:state', resolve)
+
+      aliceSocket.emit('lobby:create')
+    })
+
+    // Validate the returned lobby state
+    expect(state).toEqual({
+      id: aliceData.id,
+      ownerId: aliceData.id,
+      status: 'open',
+      maxPlayers: 4,
+      currentPlayerCount: 1,
+      players: {
+        [aliceData.id]: {
+          id: aliceData.id,
+          username: aliceData.username,
+          status: 'not-ready',
+          result: { score: 0, time: 0 },
+        },
+      },
+    })
+
+    aliceSocket.disconnect()
+  })
+})
---
+++ server/__tests__/app/sockets/protected/lobby/join.test.ts
@@ -0,0 +1,82 @@
+import type { Socket } from 'socket.io-client'
+import type { IUser } from '../../../../../src/models'
+import { buildTestServer, TestUsers } from '../../../../utils'
+
+describe('Protected Socket Namespace lobby:create', () => {
+  const server = buildTestServer()
+
+  let users: TestUsers
+
+  let aliceSocket: Socket
+  let aliceData: IUser
+  let patrykSocket: Socket
+  let patrykData: IUser
+
+  beforeAll(async () => {
+    await server.init()
+    users = new TestUsers(server.url)
+
+    const [alice, patryk] = await Promise.all([
+      users.getLoggedUser('alice'),
+      users.getLoggedUser('patryk'),
+    ])
+
+    aliceData = (await alice.me()).body.data
+    patrykData = (await patryk.me()).body.data
+
+    aliceSocket = await alice.connectProtectedSocket()
+    patrykSocket = await patryk.connectProtectedSocket()
+  })
+
+  afterAll(async () => {
+    await server.close()
+    aliceSocket.disconnect()
+  })
+
+  it('should join a lobby successfully', async () => {
+    // Listen for the lobby state after creation
+    const lobby = await new Promise<any>(resolve => {
+      aliceSocket.once('lobby:state', resolve)
+
+      aliceSocket.emit('lobby:create')
+    })
+
+    const [aliceState, patrykState] = await Promise.all([
+      new Promise(resolve => aliceSocket.once('lobby:state', resolve)),
+      new Promise(resolve => {
+        patrykSocket.once('lobby:state', resolve)
+
+        patrykSocket.emit('lobby:join', lobby.id)
+      }),
+    ])
+
+    const expectedState = {
+      id: lobby.id,
+      ownerId: aliceData.id,
+      status: 'open',
+      maxPlayers: 4,
+      currentPlayerCount: 2,
+      players: {
+        [patrykData.id]: {
+          id: patrykData.id,
+          username: patrykData.username,
+          status: 'not-ready',
+          result: { score: 0, time: 0 },
+        },
+        [aliceData.id]: {
+          id: aliceData.id,
+          username: aliceData.username,
+          status: 'not-ready',
+          result: { score: 0, time: 0 },
+        },
+      },
+    }
+
+    // Validate the returned lobby state
+    expect(aliceState).toEqual(expectedState)
+    expect(patrykState).toEqual(expectedState)
+
+    aliceSocket.disconnect()
+    patrykSocket.disconnect()
+  })
+})
