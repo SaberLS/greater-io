2116d16fefeee5117fc1b9423f6b8c95102c0680
SaberLS <patryk.platek19@gmail.com>
2026-02-13 19:40:34 +0100

    test(server/utils): add buildTestUser and unpackResData helpers
    Introduce buildTestUser to execute selected async TestUser methods
    and unpackResData to automatically assert success and extract response
    data from SuperResponse results.
    
    These helpers reduce boilerplate in integration tests and centralize
    response unwrapping and success validation logic.

---
+++ server/__tests__/utils/prepare/TestUser/buildTestUser.ts
@@ -0,0 +1,95 @@
+import type { IApiResponse, IApiSuccess } from '../../../../src/models'
+import { isSupertestResponse } from '../../isSupertestResponse'
+import type { AssertSuccessInRes, AsyncMethods } from '../../types'
+import type { SuperResponse, TestUser } from './TestUser'
+
+async function buildTestUser<
+  TUser extends TestUser,
+  TMethod extends keyof AsyncMethods<TUser>,
+>(
+  user: TUser,
+  ...methods: TMethod[]
+): Promise<ExecutedUserMethods<TUser, TMethod>> {
+  const entries = await Promise.all(
+    //@ts-expect-error this is impossible to type typescript loses link between key(TMethod) and user method TUser[TMethod]
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    methods.map(async (method: TMethod) => [method, await user[method]()])
+  )
+
+  const result = {
+    user,
+    ...Object.fromEntries(entries),
+  } as ExecutedUserMethods<TUser, TMethod>
+
+  return result
+}
+
+const unpackResData = <
+  TMethods extends keyof AsyncMethods<TUser>,
+  TUser extends TestUser = TestUser,
+  TRes extends ExecutedUserMethods<TUser, TMethods> = ExecutedUserMethods<
+    TUser,
+    TMethods
+  >,
+>(
+  res: TRes
+): InferDataFromSuperRes<AssertSuccessInRes<TRes>> => {
+  const response = {} as InferDataFromSuperRes<AssertSuccessInRes<TRes>>
+
+  for (const method in res as AssertSuccessInRes<TRes>) {
+    if (isSupertestResponse(res[method]))
+      if ((res[method].body as IApiResponse<unknown>).success)
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+        response[method] = res[method].body.data
+      else
+        throw new Error(`Method ${method} didn't succeed`, {
+          cause: res[method],
+        })
+    // @ts-expect-error sssss
+    else response[method] = res[method]
+  }
+
+  return response
+}
+
+type UserMethodsReturns<
+  TUser extends TestUser,
+  TMethods extends keyof AsyncMethods<TUser>,
+> = {
+  [K in TMethods]: TUser[K] extends () => Promise<unknown> ?
+    Awaited<ReturnType<TUser[K]>>
+  : never
+}
+
+type ExecutedUserMethods<
+  TUser extends TestUser,
+  TMethod extends keyof AsyncMethods<TUser>,
+> = UserMethodsReturns<TUser, TMethod> & { user: TUser }
+
+type InferDataFromSuperRes<T extends object> = {
+  [K in keyof T]: T[K] extends SuperResponse<IApiSuccess<infer TData>> ?
+    TData extends object ?
+      TData
+    : undefined
+  : T[K]
+}
+
+type TestUserMethods<TMethods extends keyof AsyncMethods<TestUser>> =
+  InferDataFromSuperRes<
+    AssertSuccessInRes<ExecutedUserMethods<TestUser, TMethods>>
+  >
+
+// EXAMPLE
+// const users = new TestUsers('s')
+
+// const builded = unpackResData<'me' | 'protectedSocket'>(
+//   await testUserExecutor(
+//     await users.getLoggedUser('patryk'),
+//     'health',
+//     'me',
+//     'protectedSocket'
+//   )
+// )
+
+export { buildTestUser, unpackResData }
+export type { ExecutedUserMethods, TestUserMethods, UserMethodsReturns }
