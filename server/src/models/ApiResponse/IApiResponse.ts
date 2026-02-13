a6227392f6b08322a2ee3b8f7990ab7f4dd454ff
SaberLS <patryk.platek19@gmail.com>
2026-02-13 19:52:23 +0100

    feat(server/types): remove data field from response type when it's typed as never

--- server/src/models/ApiResponse/IApiResponse.ts
+++ server/src/models/ApiResponse/IApiResponse.ts
@@ -1,7 +1,9 @@
-interface IApiResponse<TData> {
+import type { RemoveNever } from '../../utils'
+
+type IApiResponse<TData> = RemoveNever<{
   success: boolean
   message: string
   data: TData
-}
+}>
 
 export type { IApiResponse }
