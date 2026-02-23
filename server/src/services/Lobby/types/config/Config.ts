338bce287b87dbe90d004daf99a0a083cdceb8c5
SaberLS <patryk.platek19@gmail.com>
2026-02-23 19:47:13 +0100

    feat(types): add generic Config type definitions
    Introduce MemberTypes and LobbyTypes generics
    based on BASE domain abstractions.
    
    Add Statefull utility type to compose entities
    with their associated state.

---
+++ server/src/services/Lobby/types/config/Config.ts
@@ -0,0 +1,23 @@
+import type * as BASE from './BASE'
+
+interface MemberTypes<
+  TUser extends BASE.User,
+  TMemberStatus extends BASE.MemberStatus,
+> {
+  user: TUser
+  status: TMemberStatus
+}
+
+interface LobbyTypes<
+  TLobbyID extends BASE.LobbyID,
+  TLobbyStatus extends BASE.LobbyStatus,
+  TMember extends MemberTypes<BASE.User, BASE.MemberStatus>,
+> {
+  member: TMember
+  id: TLobbyID
+  status: TLobbyStatus
+}
+
+type Statefull<T extends object, TState> = T & { state: TState }
+
+export type { LobbyTypes, MemberTypes, Statefull }
