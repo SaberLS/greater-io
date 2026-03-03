3c40bf74eb67805c14b569acb0e98e5a159ea6aa
SaberLS <patryk.platek19@gmail.com>
2026-03-03 19:19:07 +0100

    feat(client/root): add store

---
+++ client/src/pages/root/store/store.ts
@@ -0,0 +1,21 @@
+import type { Action, ThunkAction } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
+import navReducer from '../../../Layouts/MainApp/store/navSlice'
+
+export const store = configureStore({
+  reducer: {
+    nav: navReducer,
+  },
+})
+
+// Infer the type of `store`
+export type AppStore = typeof store
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
+// Define a reusable type describing thunk functions
+export type AppThunk<ThunkReturnType = void> = ThunkAction<
+  ThunkReturnType,
+  RootState,
+  unknown,
+  Action
+>
