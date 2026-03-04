01c5ba935dd86889fd874e84e8e47e6ad4b76476
SaberLS <patryk.platek19@gmail.com>
2026-03-04 12:25:05 +0100

    feat(client/routes): add login route

--- client/src/routes/routes.ts
+++ client/src/routes/routes.ts
@@ -7,6 +7,9 @@ import {
 
 export default [
   // * matches all URLs, the ? makes it optional so it will match / as well
-  layout('mainApp.ts', [index('home.ts'), route('*', 'Catchall.tsx')]),
-  // route('login', 'Login.tsx'),
+  layout('mainApp.ts', [
+    index('home.ts'),
+    route('login', 'login.ts'),
+    route('*', 'Catchall.tsx'),
+  ]),
 ] satisfies RouteConfig
