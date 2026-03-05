f5fc5fd531eae17674ca18ead91d06368a7e6c29
SaberLS <patryk.platek19@gmail.com>
2026-03-05 11:38:50 +0100

    chore(client/socket): move protected socket away from index

--- client/src/services/sockets/index.ts
+++ client/src/services/sockets/index.ts
@@ -1,15 +1,2 @@
-import { io } from 'socket.io-client'
-import { UserSocket } from './UserSocket/UserSocket'
-
-const url = (import.meta as unknown as NodeJS.Process).env.VITE_SERVER_URL
-
-if (url === undefined) throw new Error('SERVER_URL is undefined')
-
-const protectedSocket = new UserSocket((token: string) =>
-  io(`${url}/protected`, {
-    transports: ['websocket'],
-    auth: { token },
-  })
-)
-
-export { protectedSocket }
+export * from './lobby'
+export * from './protectedSocket'
