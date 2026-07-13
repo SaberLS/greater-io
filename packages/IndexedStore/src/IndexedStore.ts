ac52b79a8024e897d8d67c8e9c3e8211b4f987a6
SaberLS <patryk.platek19@gmail.com>
2026-07-13 17:05:01 +0200

    feat(packages/indexed-store): add third map from value back to index

--- packages/IndexedStore/src/IndexedStore.ts
+++ packages/IndexedStore/src/IndexedStore.ts
@@ -6,13 +6,35 @@ class IndexedStore<
 > implements IIndexedStore<TIndex, TValue> {
   protected readonly _indexToValue = new Map<TIndex, TValue>()
   protected readonly _idToValue = new Map<TValue['id'], TValue>()
+  protected readonly _idToIndex = new Map<TValue['id'], TIndex>()
 
   deleteByIndex(index: TIndex): boolean {
-    return this._indexToValue.delete(index)
+    const value = this._indexToValue.get(index)
+
+    if (!value) return false
+
+    this._indexToValue.delete(index)
+    this._idToValue.delete(value.id)
+    this._idToIndex.delete(value.id)
+
+    return true
   }
 
   deleteById(valueId: TValue['id']): boolean {
-    return this._idToValue.delete(valueId)
+    const value = this._idToValue.get(valueId)
+
+    if (!value) return false
+
+    const index = this._idToIndex.get(valueId)
+
+    this._idToValue.delete(valueId)
+    this._idToIndex.delete(valueId)
+
+    if (index !== undefined) {
+      this._indexToValue.delete(index)
+    }
+
+    return true
   }
 
   getById(valueId: TValue['id']): TValue | undefined {
@@ -28,23 +50,29 @@ class IndexedStore<
   }
 
   assignToIndex(index: TIndex, value: TValue): TValue {
-    this._idToValue.set(value.id, value)
+    if (this._indexToValue.has(index)) throw new Error('Index already assigned')
+
+    if (this._idToValue.has(value.id)) throw new Error('Value already assigned')
+
     this._indexToValue.set(index, value)
+    this._idToValue.set(value.id, value)
+    this._idToIndex.set(value.id, index)
 
     return value
   }
 
   clear(): void {
-    this._idToValue.clear()
     this._indexToValue.clear()
+    this._idToValue.clear()
+    this._idToIndex.clear()
   }
 
   get valueCount(): number {
-    return this._idToValue.size
+    return this._indexToValue.size
   }
 
   get indexCount(): number {
-    return this._indexToValue.size
+    return this._idToValue.size
   }
 }
 
